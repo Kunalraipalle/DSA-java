946a40fc531da76038b090d1af0bd328944ba0dc
Kunalraipalle <166087807+Kunalraipalle@users.noreply.github.com>
2026-08-05 22:03:56 +0530

    Time: 7 ms (72.23%), Space: 49.9 MB (32.48%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,5 +1,26 @@
 import java.util.*;
+//solved same approch by using nested loop but runtime is 428ms so i made this solution hashmap based
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        int n = strs.length;
 
+        Map<String, List<String>> m = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+           
+            char[] c = strs[i].toCharArray();
+            Arrays.sort(c);
+            String dupli = new String(c);
+
+            m.putIfAbsent(dupli, new ArrayList<>());
+            List<String> l1 = m.get(dupli);
+            l1.add(strs[i]);
+        }
+
+        return new ArrayList<>(m.values());
+    }
+}
+/*
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         int n = strs.length;
@@ -37,3 +58,4 @@ class Solution {
         return result;
     }
 }
+*/
