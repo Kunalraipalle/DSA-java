c0c0b1ac8d617a6d45544c8b08f47e87292e99b6
Kunalraipalle <166087807+Kunalraipalle@users.noreply.github.com>
2026-08-15 23:14:51 +0530

    Time: 0 ms (100%), Space: 44.8 MB (79.78%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.java
@@ -1,6 +1,9 @@
 class Solution {
     public double angleClock(int hour, int minutes) {
-
+        /*
+          Angle = Absolute Value of (30 times Hours - 5.5 times Minutes)
+          If the answer is greater than 180 subtract it from 360.
+        */
         double angle = Math.abs(30 * hour - 5.5 * minutes);
         if (angle > 180) {
             angle = 360 - angle;
